6adb288cab55a24077562072659005fa5367c729
rafa <askunknownsurname@gmail.com>
2022-02-09 06:39:44 -0300

    luminosity dim and code organization

--- battleground.c
+++ battleground.c
@@ -1,12 +1,13 @@
 #include "minirt.h"
 
+//If point belong to circle
 int		this_point_is_in_a_circle(int i, int j, int x_position, int y_position, int radius)
 {
 	if(((i - x_position) * (i - x_position)) + ((j - y_position) * (j - y_position)) < (radius * radius))
 		return 1;
 	return 0; 
 }
-
+//If line cross line
 int line_line(int i, int j, t_data *img, t_cord cord1, t_cord cord2)
 {
     t_cord cord3;
@@ -28,55 +29,80 @@ int line_line(int i, int j, t_data *img, t_cord cord1, t_cord cord2)
         return 1;
     return 0;
 }
-
+//create lines
 t_cord  *assign_cord_line_values(void)
 {
     t_cord *cord = (t_cord *)malloc(sizeof(t_cord) * 8);
-    cord[0].x = 400;cord[0].y = 500;
-    cord[1].x = 400;cord[1].y = 400;
+    cord[0].x = 400; cord[0].y = 500;
+    cord[1].x = 400; cord[1].y = 400;
 
-    cord[2].x = 400;cord[2].y = 400;
-    cord[3].x = 500;cord[3].y = 400;
+    cord[2].x = 400; cord[2].y = 400;
+    cord[3].x = 500; cord[3].y = 400;
 
-    cord[4].x = 500;cord[4].y = 400;
-    cord[5].x = 500;cord[5].y = 500;
+    cord[4].x = 500; cord[4].y = 400;
+    cord[5].x = 500; cord[5].y = 500;
 
-    cord[6].x = 400;cord[6].y = 500;
-    cord[7].x = 500;cord[7].y = 500;
+    cord[6].x = 400; cord[6].y = 500;
+    cord[7].x = 500; cord[7].y = 500;
     return cord;
+}
+
+void color_by_distance_two_points(int x1, int y1, int x2, int y2, int *color)
+{
+    double d = sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+    int distance = (int)d;
+    int total_dist = 1100;
+    int result = mlx_get_hex_trgb(200 - (distance * 200 / total_dist), 200 - (distance * 200 / total_dist), 200 - (distance * 200 / total_dist));
+    *color = result;
+}
+
+int belong_to_a_object(int i, int j, int sx, int sy, int *color)
+{
+    //desenhar o quadrado do objeto
+    if(i >= 400 && i <= 500 && j >= 400 && j <= 500)
+        *color = mlx_get_hex_trgb(0, 255, 0);
+    //desenhar circulo na fonte de luz
+    else if(this_point_is_in_a_circle(i, j, sx, sy, 5))
+        *color = mlx_get_hex_trgb(255, 255, 0);
+    else
+        return -1;
+    return 1;
+}
+
+int behind_an_object(int i, int j, t_cord *cord, t_data *img, int *color)
+{
+    if(line_line(i, j, img, cord[0], cord[1]))
+        *color = mlx_get_hex_trgb(0, 0, 0);
+    else if(line_line(i, j, img, cord[2], cord[3]))
+        *color = mlx_get_hex_trgb(0, 0, 0);
+    else if(line_line(i, j, img, cord[4], cord[5]))
+        *color = mlx_get_hex_trgb(0, 0, 0);
+    else if(line_line(i, j, img, cord[5], cord[6]))
+        *color = mlx_get_hex_trgb(0, 0, 0);
+    else
+        return -1;
+    return 1;
 }
 
 void	draw(t_data *img)
 {
-	int i = 0;
-	int j = 0;
-	int color = 0;
     t_cord *cord;
+    int color;
+    int i;
+	int j = -1;
 
     cord = assign_cord_line_values();
-	while(j < 900)
+	while(++j < 900)
 	{
+        i = 0;
 		while(i < 900)
 		{	
-            if(i >= 400 && i <= 500 && j >= 400 && j <= 500)
-                color = mlx_get_hex_trgb(0, 255, 0);
-            else if(this_point_is_in_a_circle(i, j, img->x_position, img->y_position, 5))
-                color = mlx_get_hex_trgb(255, 255, 0);
-            else if(line_line(i, j, img, cord[0], cord[1]))
-                color = mlx_get_hex_trgb(0, 0, 0);
-            else if(line_line(i, j, img, cord[2], cord[3]))
-                color = mlx_get_hex_trgb(0, 0, 0);
-            else if(line_line(i, j, img, cord[4], cord[5]))
-                color = mlx_get_hex_trgb(0, 0, 0);
-            else if(line_line(i, j, img, cord[5], cord[6]))
-                color = mlx_get_hex_trgb(0, 0, 0);
+            if      (belong_to_a_object(i, j, img->x_position, img->y_position, &color) != -1);
+            else if (behind_an_object(i, j, cord, img, &color) != -1);
 			else
-                color = mlx_get_hex_trgb(200, 200, 200);
-			my_mlx_pixel_put(img, i, j, color);
-			i++;
+                    color_by_distance_two_points(i, j, img->x_position, img->y_position, &color);
+			my_mlx_pixel_put(img, i++, j, color);
 		}
-		i = 0;
-		j++;
 	}
     free(cord);
 	mlx_put_image_to_window(img->mlx, img->mlx_win, img->img, 0, 0);
--- main.c
+++ main.c
@@ -35,53 +35,65 @@ int	key_control(int key, t_data *img)
 	return (0);
 }
 
-int	main(void)
+void	animation(t_data *img)
 {
-	t_data	img;
 	int to_right = 1;
 	int to_bottom = 0;
 	int to_left = 0;
 	int to_up = 0;
-	img.radius = 50;
-	img.x_position = 0;
-	img.y_position = 50;
-	img.div = 900;
-	img.mlx = mlx_init();
-	img.mlx_win = mlx_new_window(img.mlx, 900, 900, "Minirt");
-	img.img = mlx_new_image(img.mlx, 900, 900);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	while(img.x_position < 902)
+	while(img->x_position < 902)
 	{
-		draw(&img);
-		if(img.x_position == 850)
+		draw(img);
+		if(img->x_position == 850)
 		{
 			to_right = 0;
 			to_bottom = 1;
 		}
-		if(img.y_position == 850)
+		if(img->y_position == 850)
 		{
 			to_bottom = 0;
 			to_left = 1;
 		}
-		if(img.x_position == 50)
+		if(img->x_position == 50)
 		{
 			to_left = 0;
 			to_up = 1;
 		}
-		if(img.y_position == 50 && img.x_position == 50)
+		if(img->y_position == 50 && img->x_position == 50)
 		{
 			to_up = 0;
 			to_right = 1;
 		}
 		if(to_right)
-			img.x_position += 1;
+			img->x_position += 1;
 		else if (to_bottom)
-			img.y_position += 1;
+			img->y_position += 1;
 		else if (to_left)
-			img.x_position -= 1;
+			img->x_position -= 1;
 		else if (to_up)
-			img.y_position -= 1;
+			img->y_position -= 1;
 	}
+}
+
+void	set_img(t_data *img)
+{
+	img->radius = 50;
+	img->x_position = 0;
+	img->y_position = 50;
+	img->div = 900;
+}
+
+int	main(void)
+{
+	t_data	img;
+
+	set_img(&img);
+	img.mlx = mlx_init();
+	img.mlx_win = mlx_new_window(img.mlx, 900, 900, "2dMinirt");
+	img.img = mlx_new_image(img.mlx, 900, 900);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	animation(&img);
+	//draw(&img);
 	mlx_key_hook(img.mlx_win, key_control, &img);
 	mlx_loop(img.mlx);
 }
